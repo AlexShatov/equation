12383d198e72f257aa20a0396489436488a019cf
Alexander Shatov <alx18486@gmail.com>
2019-09-17 21:13:20 +0300

    fixed

--- src/test/java/ru/equation/EquationSolverTest.java
+++ src/test/java/ru/equation/EquationSolverTest.java
@@ -1,40 +1,39 @@
 package ru.equation;
 
 import org.junit.*;
-import static org.junit.Assert.*;
 import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class EquationSolverTest {
 	
-	public EquationSolverTest() {
-		
-	}
-	
-	private static EquationSolver solver;
-	private ArrayList<Double> solArray;
-	
-	@BeforeClass
-	public static void init() {
-		solver = new EquationSolver();
+	@Test
+	public void testNullSolution() {
+		EquationSolver solver = new EquationSolver();
+		assertNull(solver.solve(2.1, 4.08, 7.26));
 	}
 	
 	@Test
-	public void testSolve() throws Exception{
-		solArray = solver.solve(2.1, 4.08, 7.26);
-		assertNull(solArray);
-		solArray = solver.solve(1.0, 12.0, 36.0);
-		assertEquals(1 ,solArray.size());
-		assertEquals((Double)(-6.0), solArray.get(0));
-		solArray = solver.solve(1.0, -2.0, -3.0);
-		assertEquals(2, solArray.size());
-		assertEquals((Double)3.0, solArray.get(0));
-		assertEquals((Double)(-1.0), solArray.get(1));
-	}
-
-	@Test(expected = Exception.class)
-	public void testSolveException() throws Exception{
-		solver.solve(0.0, 4.08, 7.26);
+	public void testOneSolution() {
+		EquationSolver solver = new EquationSolver();
+		ArrayList <Double> solList = solver.solve(1.0, 12.0, 36.0);
+		assertEquals(1 , solList.size());
+		assertEquals((Double)(-6.0), solList.get(0));
 	}
 	
+	@Test
+	public void testTwoSolutions() {
+		EquationSolver solver = new EquationSolver();
+		ArrayList <Double> solList = solver.solve(1.0, -2.0, -3.0);
+		assertEquals(2, solList.size());
+		assertEquals((Double)3.0, solList.get(0));
+		assertEquals((Double)(-1.0), solList.get(1));
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testSolveException() throws Exception{
+		EquationSolver solver = new EquationSolver();
+		solver.solve(0.0, 4.08, 7.26);
+	}
 
 }
